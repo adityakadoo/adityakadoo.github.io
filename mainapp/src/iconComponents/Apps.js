3c981add048e1cc1bb90be28b76a96ad9b94258d
adityakadoo <aditya1449kadoo@gmail.com>
2022-12-25 17:37:59 +0530

    Ah shit, here we go again.

--- mainapp/src/iconComponents/Apps.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const SvgApps = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="1em" width="1em" {...props}>
-    <path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z" fill="currentColor" stroke="currentColor" />
-  </svg>
-);
-
-export default SvgApps;
